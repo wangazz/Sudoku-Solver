928385a1bc4debf83741eaae245747b9497c1b4f
Andy Wang <andywang.16@gmail.com>
2019-11-28 22:28:25 +0000

    Added C# files

---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sudoku_Solver
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input:");
+            string inputString = "";
+            while (inputString.Length != 81) {
+                inputString = Console.ReadLine();
+                if (inputString.Length != 81) {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+            ProcessInputs(inputString);
+        }
+
+        private static void ProcessInputs(string inputString)
+        {
+            int[] inputArray = new int[81];
+            
+        }
+    }
+}
